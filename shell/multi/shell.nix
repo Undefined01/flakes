bbd4b94141c88dc8bed94c1d0ed9c35ceecda001
Undefined01 <amoscr@163.com>
2023-07-22 14:06:11 +0800

    Add shell

---
+++ shell/multi/shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    gcc
+    gnumake
+    cargo
+    rustc
+    nodejs
+    jdk17_headless
+    maven
+    kotlin
+  ];
+}
+
