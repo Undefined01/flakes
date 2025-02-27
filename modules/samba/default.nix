1a86db480c9a314f36fdb87d70a6df91cd47b309
Undefined01 <amoscr@163.com>
2025-02-27 15:30:18 +0800

    Update

--- modules/samba/default.nix
+++ modules/samba/default.nix
@@ -4,19 +4,27 @@
     openFirewall = true;
     settings = {
       global = {
-        "invalid users" = [
-          "root"
-        ];
-        "passwd program" = "/run/wrappers/bin/passwd %u";
         security = "user";
+        "invalid users" = [ "root" ];
+        "passwd program" = "/run/wrappers/bin/passwd %u";
+        "guest account" = "nobody";
         "hosts allow" = "0.0.0.0/0";
+      };
+      home = {
+        browseable = "yes";
+        comment = "Home directory for %u";
+        path = "/home/%u";
+        "read only" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
       };
       public = {
         browseable = "yes";
         comment = "Public samba share.";
-        "guest ok" = "yes";
         path = "/srv/public";
-        "read only" = "no";
+        "guest ok" = "yes";
+        "force user" = "nobody";
+        "read only" = "yes";
         "create mask" = "0644";
         "directory mask" = "0755";
       };
