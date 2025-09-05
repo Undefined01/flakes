ca451f81e2d9dc720cdc78eec6f3f791fd346e29
Undefined01 <amoscr@163.com>
2025-09-05 14:42:24 +0800

    Update

---
+++ shell/c/shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> { system="aarch64-darwin"; } }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    gcc
+    gdb
+    lldb
+    gnumake
+  ];
+}
+
--- shell/multi/shell.nix
+++ shell/multi/shell.nix
@@ -3,6 +3,7 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     gcc
+    gdb
     gnumake
     cmake
     rustc
