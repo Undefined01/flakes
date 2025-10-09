9626f77ad6f429f959fa596003a8a8e4d570831f
Undefined01 <amoscr@163.com>
2025-10-09 22:10:58 +0800

    update

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,12 @@
+{ user, pkgs, ... }:
+
 {
   imports = [
     ./common.nix
     ./presets/commandline
     ./presets/desktop
   ];
+
+  home.username = user;
+  home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${user}" else "/home/${user}";
 }
