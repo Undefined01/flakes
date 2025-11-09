4a0a1351b2394c4d2a8dccbe9913f99be6f826b7
Undefined01 <amoscr@163.com>
2025-11-09 21:07:36 +0800

    Update

--- configurations/shared/nixos/default.nix
+++ configurations/shared/nixos/default.nix
@@ -26,6 +26,7 @@
     users.root.initialPassword = "${user}";
     users.${user} = {
       isNormalUser = true;
+      group = "${user}";
       extraGroups = [ "wheel" "users" "${user}" ];
       initialPassword = "${user}";
       openssh.authorizedKeys.keys = [
