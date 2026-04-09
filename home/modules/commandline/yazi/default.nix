f37583a7c9355704b5adb892e2bd2128e13ab09d
Undefined01 <amoscr@163.com>
2026-04-09 20:29:42 +0800

    Record directory history for yazi in fish
    Override the generated fish wrapper so the shell uses the plain cd command after reading the target directory, allowing the shell to record directory history. Use a temporary file to receive the cwd and clean up afterwards.

--- home/modules/commandline/yazi/default.nix
+++ home/modules/commandline/yazi/default.nix
@@ -1,3 +1,5 @@
+{ config, lib, ... }:
+
 {
   programs.yazi = {
     enable = true;
@@ -12,4 +14,19 @@
 
     shellWrapperName = "y";
   };
+
+  # Override the `builtin cd` to `cd` to record the directory history in fish
+  # Source:
+  #   https://github.com/nix-community/home-manager/blob/0d02ec1d0a05f88ef9e74b516842900c41f0f2fe/modules/programs/yazi.nix#L235
+  # Related issues:
+  #   https://github.com/sxyazi/yazi/discussions/1964
+  #   https://github.com/sxyazi/yazi/issues/2859
+  programs.fish.functions.${config.programs.yazi.shellWrapperName} = lib.mkForce ''
+    set -l tmp (mktemp -t "yazi-cwd.XXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+      cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+  '';
 }
