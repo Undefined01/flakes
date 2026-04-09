527c962e3e965eeba516a84275435878a70e13e3
Undefined01 <amoscr@163.com>
2026-04-09 20:43:16 +0800

    update neovim options

--- home/modules/commandline/neovim/default.nix
+++ home/modules/commandline/neovim/default.nix
@@ -4,6 +4,9 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+
+    withRuby = false;
+    withPython3 = false;
   };
 
   xdg.configFile.nvim = lib.mkIf (builtins.pathExists ./nvim-config) {
